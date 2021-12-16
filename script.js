0b7809d7af65462f3c0df05c47c84fe62f6db7af
ginkoemdon <emdo0001@algonquinlive.com>
2021-12-15 19:57:27 -0500

    Second Commit

--- script.js
+++ script.js
@@ -18,41 +18,140 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=vgqGnJL5WoracovhddpnoaxTdCrU7
 
 //----------- GLOBAL DECLARATIONS
 const $24hour = document.getElementById('24hour')
+const $12hours = document.getElementById('12hours')
+const $clockContrast = document.getElementById('contrast')
 const $container = document.getElementById('container')
 const $save = document.getElementById('save')
-
+const $seconds = document.getElementById('seconds')
+const $greetings = document.getElementById('greetings')
 //-----------------------------------------------------
 
 
-//----------- CLOCK
+//----------- CLOCK SET INTERVAL
 setInterval(function () {
     const now = new Date()
 
     // if checked then display the value 
     if (control24Clock) {
-        $container.innerText = now.toLocaleTimeString('en-GB')
-    } else {
+        if (showSeconds == true){ 
+            $container.innerText = now.toLocaleTimeString('en-GB')   
+        }
+        else {
+            $container.innerText = now.getHours()+":"+now.getMinutes()
+        }
+
+    } else 
+         if (showSeconds == true)
+    {
         $container.innerText = now.toLocaleTimeString('en-US')
+    } 
+    else{
+        if (now.getHours() < 12){
+            $container.innerText = now.getHours()+":"+now.getMinutes()+"am"
+        } else {$container.innerText = (now.getHours() - 12)+":"+now.getMinutes()+"pm"}
     }
+
+     // GREETINGS 
+     if (now.getHours() < 12){
+         $greetings.innerHTML = "Good Morning, Earthling"
+     }
+
+     if (now.getHours() > 12){
+        $greetings.innerHTML = "Good Afternoon, Tellurian"
+        console.log($greetings)
+    }
+
+    // greater than 5pm
+    if (now.getHours() > 17){
+        $greetings.innerHTML = "Good Evening, Terrestrial Being"
+        console.log($greetings)
+    }
+    
     console.log('Clock => ', now)
 }, 1000)
 
-// Date.prototype.toDateString()
+//----------- end Clock
 
-// 12hour time | US English: 12-hour time with AM/PM → "7:00:00 PM"
-console.log(now.toLocaleTimeString('en-US'));
+let control24Clock = false
+let setClockContrast = false
+let showSeconds = false
 
-// 24hour time | British English: 24-hour time without AM/PM → "03:00:00"
-console.log(now.toLocaleTimeString('en-GB'));
+//----------- LOCAL STORAGE
+// To check if local storage has the settings values 
+if(localStorage.getItem('24HourSetting')){
+    // Assigning whatever the localStorage has
+    // taking the variable from local storage and asigning the value to 24 hour clock
+    control24Clock = localStorage.getItem('24HourSetting')
+    setClockContrast = localStorage.getItem('Contrast')
+    showSeconds = localStorage.getItem('Seconds')
+
+    //---- 24 Hour Clock (Local Storage) ---- 
+    if(control24Clock == 'true'){
+        $24hour.checked = true
+        control24Clock = true
+    } else{
+        $24hour.checked = false
+        control24Clock = false
+    }
+
+    //---- Contrast (Local Storage) ---- 
+    if(setClockContrast == 'true'){
+        $container.classList.add('contrast')
+         $greetings.classList.add('contrast')
+        $clockContrast.checked = true
+        setClockContrast = true
+    } else{
+        $container.classList.remove('contrast')
+        $greetings.classList.remove('contrast')
+        $clockContrast.checked = false
+        setClockContrast = false
+    }
+
+    //---- Showing Seconds (Local Storage) ---- 
+    if(showSeconds == 'true'){
+        $seconds.checked = true
+        showSeconds = true
+    } else{
+        $seconds.checked = false
+        showSeconds = false
+    }
+}
 
 
 //----------- SETTINGS
-let control24Clock = false
-    $submit.addEventListener('click', function () {
-        // change control variables
-        control24Clock = true
+
+    //---- Event Listener for Settings Checkbox ---- 
+    $save.addEventListener('click', function(e) {
+        e.preventDefault();
+
+    // change settings variables
+
+        // 24hour time | British English: 24-hour time without AM/PM → "03:00:00"
+        control24Clock = $24hour.checked
+        console.log(control24Clock)
+
+        //---- Contrast for Clock Background  ----
+        setClockContrast = $clockContrast.checked
+        if (setClockContrast == true) {
+            $container.classList.add('contrast')
+            $greetings.classList.add('contrast')
+        }
+        else {
+        $container.classList.remove('contrast')
+        $greetings.classList.remove('contrast')
+        }
+
+        //---- Showing Seconds ----
+        showSeconds = $seconds.checked
+
+          //---- Local Storage  ----
+        localStorage.setItem('24HourSetting', control24Clock)
+        localStorage.setItem('Contrast', setClockContrast)
+        localStorage.setItem('Seconds', showSeconds)
     })
 
-//SHOW DATE
+// // ------------------
 
-//----------- END CLOCK
+
+
+//SHOW DATE
